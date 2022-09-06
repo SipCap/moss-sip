83a8d6fe9833af0eeee04515ae9859441f140be5
SipCap <59588929+SipCap@users.noreply.github.com>
2022-09-06 15:41:06 -0400

    moss_fix_solo_by_sip.bat

--- moss_fix_solo_by_sip.bat
+++ moss_fix_solo_by_sip.bat
@@ -1,5 +1,4 @@
 @ECHO off
-::by sippy [twitter.com/siipcap]
 title Sippy Moss fix
 ECHO Applying fix. . .
 reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Image File Execution Options\MossX64.exe\PerfOptions" /v "CpuPriorityClass" /t reg_DWORD /d "1" /f
